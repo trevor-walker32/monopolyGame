1dd553dd09f722272ed8ce337ce777f4baaf662e
Trevor Walker <35089088+trevor-walker32@users.noreply.github.com>
2019-03-02 14:12:51 -0800

    updated the board

--- display.cpp
+++ display.cpp
@@ -2,31 +2,30 @@
 
 
 // Display the board
-// Please do not overflow the buffers... 
 
-char board0[] = "|F|x|x|C|x|R|x|x|x|x|J|";
-char board1[] = "|x|    m            |x|";
-char board2[] = "|C|     o           |x|";
-char board3[] = "|x|      n          |C|";
-char board4[] = "|x|       o         |x|";
+char board0[] = "|F| | |C| |R| | | | |J|";
+char board1[] = "| |    m            | |";
+char board2[] = "|C|     o           | |";
+char board3[] = "| |      n          |C|";
+char board4[] = "| |       o         | |";
 char board5[] = "|R|        p        |R|";
-char board6[] = "|x|         o       |C|";
-char board7[] = "|x|          l      |x|";
-char board8[] = "|x|           y     |T|";
-char board9[] = "|x|                 |x|";
-char board10[]= "|J|x|x|C|x|R|T|x|C|x|G|";
+char board6[] = "| |         o       |C|";
+char board7[] = "| |          l      | |";
+char board8[] = "| |           y     |T|";
+char board9[] = "| |                 | |";
+char board10[]= "|J| | |C| |R|T| |C| |G|";
 
-char board11[] = "|F|x|x|C|x|R|x|x|x|x|J|";
-char board12[] = "|x|    m            |x|";
-char board13[] = "|C|     o           |x|";
-char board14[] = "|x|      n          |C|";
-char board15[] = "|x|       o         |x|";
+char board11[] = "|F| | |C| |R| | | | |J|";
+char board12[] = "| |    m            | |";
+char board13[] = "|C|     o           | |";
+char board14[] = "| |      n          |C|";
+char board15[] = "| |       o         | |";
 char board16[] = "|R|        p        |R|";
-char board17[] = "|x|         o       |C|";
-char board18[] = "|x|          l      |x|";
-char board19[] = "|x|           y     |T|";
-char board20[] = "|x|                 |x|";
-char board21[]=  "|J|x|x|C|x|R|T|x|C|x|G|";
+char board17[] = "| |         o       |C|";
+char board18[] = "| |          l      | |";
+char board19[] = "| |           y     |T|";
+char board20[] = "| |                 | |";
+char board21[]=  "|J| | |C| |R|T| |C| |G|";
 
 
 
